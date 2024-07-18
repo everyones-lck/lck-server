28764cb0dee9f47819d2894625d575f3f753861c
박준영 <wnsdud6969@naver.com>
2024-07-18 18:03:06 +0900

    Add: initialize project

---
+++ src/main/java/com/lckback/lckforall/LckForAllApplication.java
@@ -0,0 +1,13 @@
+package com.lckback.lckforall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LckForAllApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LckForAllApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lckback/lckforall/LckForAllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lckback.lckforall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LckForAllApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
