b6c7c1926f8b70aa1dd6f44b97582b7f0a275458
gimgyumin <qmean59@gmail.com>
2024-07-27 05:18:10 +0900

    Feat(AboutMatchControllerDto) : add AboutMatchControllerDto

---
+++ src/main/java/com/lckback/lckforall/aboutlck/dto/AboutMatchControllerDto.java
@@ -0,0 +1,36 @@
+package com.lckback.lckforall.aboutlck.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import lombok.Builder;
+import lombok.Getter;
+
+public class AboutMatchControllerDto {
+
+	@Getter
+	@Builder
+	public static class Response {
+		private List<MatchDetail> matchDetailList;
+		private Integer listSize;
+	}
+
+	@Getter
+	@Builder
+	public static class MatchDetail {
+		private TeamDetail team1;
+		private TeamDetail team2;
+		private boolean matchFinished;
+		private String season;
+		private Integer matchNumber;
+		private LocalDate matchDate;
+	}
+
+	@Getter
+	@Builder
+	public static class TeamDetail {
+		private String teamName;
+		private String teamLogoUrl;
+		private boolean isWinner;
+	}
+}
