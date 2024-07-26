2204e3f947e445765609cff5785c6dd38dbf1e6d
gimgyumin <qmean59@gmail.com>
2024-07-27 04:50:41 +0900

    Fix(MatchRepository) : add EntityGraph in Function

--- src/main/java/com/lckback/lckforall/aboutlck/repository/MatchRepository.java
+++ src/main/java/com/lckback/lckforall/aboutlck/repository/MatchRepository.java
@@ -3,10 +3,12 @@ package com.lckback.lckforall.aboutlck.repository;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.lckback.lckforall.match.model.Match;
 
 public interface MatchRepository extends JpaRepository<Match, Long> {
+	@EntityGraph(attributePaths = {"team1", "team2"})
 	List<Match> findMatchesByMatchDateBetween(LocalDateTime start, LocalDateTime end);
 }
