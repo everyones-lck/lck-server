fcc10ac7a7fe486413ea3784f01c72bc9fba2f2f
JunYeong Park <101688752+farmJun@users.noreply.github.com>
2024-07-30 23:17:20 +0900

    [Feat/#27] get user's profile (#32)
    * Feat(UserRepository): add user entity repository
    
    * Feat(User): add update method of nickname, profile image url
    
    * Feat(GetUserProfileDto): add dto for get user's profile
    
    * Feat(MyPageService): add MyPageService and implement getUserProfile()
    
    * Feat(MyPageController): add MyPageController and implement getUserProfile()

---
+++ src/main/java/com/lckback/lckforall/user/respository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.lckback.lckforall.user.respository;
+
+import com.lckback.lckforall.user.model.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
