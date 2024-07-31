c276201564f49b58f06357d594b596b8bd260b66
gimgyumin <qmean59@gmail.com>
2024-07-31 18:37:38 +0900

    Feat: add dto for api request and response

---
+++ src/main/java/com/lckback/lckforall/aboutlck/dto/FindTeamPlayerHistoryDto.java
@@ -0,0 +1,48 @@
+package com.lckback.lckforall.aboutlck.dto;
+
+
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+
+import com.lckback.lckforall.base.type.PlayerPosition;
+import com.lckback.lckforall.base.type.PlayerRole;
+
+import lombok.Builder;
+import lombok.Getter;
+
+public class FindTeamPlayerHistoryDto {
+	@Getter
+	@Builder
+	public static class Parameter {
+		private Pageable pageable;
+		private Long teamId;
+	}
+
+	@Getter
+	@Builder
+	public static class Response {
+		private List<SeasonDetail> seasonDetails;
+		private Integer totalPage;
+		private Long totalElements;
+		private Boolean isFirst;
+		private Boolean isLast;
+	}
+
+	@Getter
+	@Builder
+	public static class SeasonDetail {
+		private List<PlayerDetail> players;
+		private Integer listSize;
+		private String seasonName;
+	}
+
+	@Getter
+	@Builder
+	public static class PlayerDetail {
+		private Long playerId;
+		private String playerName;
+		private PlayerRole playerRole;
+		private PlayerPosition playerPosition;
+	}
+}
---
+++ src/main/java/com/lckback/lckforall/aboutlck/dto/FindTeamWinningHistoryDto.java
@@ -0,0 +1,27 @@
+package com.lckback.lckforall.aboutlck.dto;
+
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+
+import lombok.Builder;
+import lombok.Getter;
+
+public class FindTeamWinningHistoryDto {
+	@Getter
+	@Builder
+	public static class Parameter {
+		private Long teamId;
+		private Pageable pageable;
+	}
+
+	@Getter
+	@Builder
+	public static class Response {
+		private List<String> seasonNameList;
+		private Integer totalPage;
+		private Long totalElements;
+		private Boolean isFirst;
+		private Boolean isLast;
+	}
+}
