1d0f7532ecbbc6b354092d3352783bbcf3a4f9a8
gimgyumin <qmean59@gmail.com>
2024-07-31 18:41:42 +0900

    Fix: fix requestMapping api path

--- src/main/java/com/lckback/lckforall/aboutlck/controller/AboutLckMatchController.java
+++ src/main/java/com/lckback/lckforall/aboutlck/controller/AboutLckMatchController.java
@@ -3,6 +3,7 @@ package com.lckback.lckforall.aboutlck.controller;
 import java.time.LocalDate;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,13 +13,14 @@ import com.lckback.lckforall.base.api.ApiResponse;
 
 import lombok.RequiredArgsConstructor;
 
-@RestController("/aboutlck/match")
+@RestController
+@RequestMapping("/aboutlck/match")
 @RequiredArgsConstructor
 public class AboutLckMatchController {
 
 	private final AboutLckMatchService aboutLckMatchService;
 
-	@GetMapping("/")
+	@GetMapping
 	public ApiResponse<FindMatchesByDateDto.Response> findMatchInformationByDate(
 		@RequestParam("searchDate") LocalDate searchDate) {
 		FindMatchesByDateDto.Parameter param = FindMatchesByDateDto.Parameter.builder().searchDate(searchDate).build();
