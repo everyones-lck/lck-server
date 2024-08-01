544a212418d5a1141a0733359e1bbd4cd4ab687a
shchoilv1 <bychoise8@gmail.com>
2024-08-01 15:38:11 +0900

    [FEAT] API 예외처리 구현

--- src/main/java/com/lckback/lckforall/base/api/handler/ExceptionAdvice.java
+++ src/main/java/com/lckback/lckforall/base/api/handler/ExceptionAdvice.java
@@ -1,4 +1,24 @@
 package com.lckback.lckforall.base.api.handler;
 
+import com.lckback.lckforall.base.api.ApiResponse;
+import com.lckback.lckforall.base.api.error.CommonErrorCode;
+import com.lckback.lckforall.base.api.exception.RestApiException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
 public class ExceptionAdvice {
+
+    @ExceptionHandler(RestApiException.class)
+    public ApiResponse<?> handleRestApiException(RestApiException e) {
+        return ApiResponse.createFail(e.getErrorCode());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ApiResponse<?> handleException() {
+        return ApiResponse.createFail(CommonErrorCode.INTERNAL_SERVER_ERROR);
+    }
 }
+
