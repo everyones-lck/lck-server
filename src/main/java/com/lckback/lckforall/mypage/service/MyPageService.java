6234b3d64ea20bcd501e65fa01f63a75dfafaa71
박준영 <wnsdud6969@naver.com>
2024-08-01 21:48:42 +0900

    Fix(MyPageService): add `@Transactional`

--- src/main/java/com/lckback/lckforall/mypage/service/MyPageService.java
+++ src/main/java/com/lckback/lckforall/mypage/service/MyPageService.java
@@ -9,8 +9,10 @@ import com.lckback.lckforall.user.respository.UserRepository;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class MyPageService {
 
