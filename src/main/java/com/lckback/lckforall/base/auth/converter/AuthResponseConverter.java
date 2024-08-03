f9c65f8a9ac9bbcb2c1c378cd8244cb65149e720
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-08-04 03:58:42 +0900

    Feat(AuthResponseConverter): convert tokens into AuthResponseDto

---
+++ src/main/java/com/lckback/lckforall/base/auth/converter/AuthResponseConverter.java
@@ -0,0 +1,13 @@
+package com.lckback.lckforall.base.auth.converter;
+
+import com.lckback.lckforall.base.auth.dto.AuthResponseDto;
+
+public class AuthResponseConverter {
+
+	// accessToken과 refreshToken을 받아 AuthResponseDto 객체로 변환하는 역할
+	public static AuthResponseDto convertToAuthResponseDto(String accessToken, String refreshToken) {
+
+		return new AuthResponseDto(accessToken, refreshToken);
+	}
+
+}
