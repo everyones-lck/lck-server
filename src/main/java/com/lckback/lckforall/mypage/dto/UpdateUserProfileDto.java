66806c71a01005528a8b6079a355f11e3aa20d67
박준영 <wnsdud6969@naver.com>
2024-08-03 22:02:47 +0900

    Feat(UpdateUserProfileDto): add DTO for update user's profile

---
+++ src/main/java/com/lckback/lckforall/mypage/dto/UpdateUserProfileDto.java
@@ -0,0 +1,28 @@
+package com.lckback.lckforall.mypage.dto;
+
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.Pattern;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class UpdateUserProfileDto {
+
+    @NoArgsConstructor
+    @Getter
+    public static class Request {
+
+        @Nullable
+        @Pattern(regexp = "^(\\S){1,10}$", message = "닉네임은 최소 1자에서 최대 10자, 공백이 없어야 합니다.")
+        private String nickname;
+    }
+
+    @Builder
+    @Getter
+    public static class Response {
+
+        private String updatedProfileImageUrl;
+        private String updatedNickname;
+    }
+}
