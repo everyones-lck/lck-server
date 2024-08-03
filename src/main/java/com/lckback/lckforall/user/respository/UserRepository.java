3047e5713bc51151fb82559d98031ad051a7753f
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-08-04 03:22:51 +0900

    Feat(UserRepository): add findByKakaoUserId related to jwt logic

--- src/main/java/com/lckback/lckforall/user/respository/UserRepository.java
+++ src/main/java/com/lckback/lckforall/user/respository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.lckback.lckforall.user.respository;
 
+import java.util.Optional;
+
 import com.lckback.lckforall.user.model.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+	Optional<User> findByKakaoUserId(String kakaoUserId);
 }
