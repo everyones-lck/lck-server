84162252734e2c1a5060c62c73a92bdd533d0ad5
박준영 <wnsdud6969@naver.com>
2024-08-04 20:04:02 +0900

    Feat(UpdateUserProfileDto.Request): add field isDefaultImage

--- src/main/java/com/lckback/lckforall/mypage/dto/UpdateUserProfileDto.java
+++ src/main/java/com/lckback/lckforall/mypage/dto/UpdateUserProfileDto.java
@@ -9,20 +9,22 @@ import lombok.NoArgsConstructor;
 
 public class UpdateUserProfileDto {
 
-    @NoArgsConstructor
-    @Getter
-    public static class Request {
+	@NoArgsConstructor
+	@Getter
+	public static class Request {
 
-        @Nullable
-        @Pattern(regexp = "^(\\S){1,10}$", message = "닉네임은 최소 1자에서 최대 10자, 공백이 없어야 합니다.")
-        private String nickname;
-    }
+		@Nullable
+		@Pattern(regexp = "^(\\S){1,10}$", message = "닉네임은 최소 1자에서 최대 10자, 공백이 없어야 합니다.")
+		private String nickname;
 
-    @Builder
-    @Getter
-    public static class Response {
+		private boolean isDefaultImage;
+	}
 
-        private String updatedProfileImageUrl;
-        private String updatedNickname;
-    }
+	@Builder
+	@Getter
+	public static class Response {
+
+		private String updatedProfileImageUrl;
+		private String updatedNickname;
+	}
 }
