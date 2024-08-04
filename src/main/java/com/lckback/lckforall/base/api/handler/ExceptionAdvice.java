93f21c46cc56898ab043132002fcad9cf45ae049
shchoilv1 <bychoise8@gmail.com>
2024-08-04 19:28:19 +0900

    [FEAT]: 에러핸들러 추가 구현

--- src/main/java/com/lckback/lckforall/base/api/handler/ExceptionAdvice.java
+++ src/main/java/com/lckback/lckforall/base/api/handler/ExceptionAdvice.java
@@ -2,23 +2,74 @@ package com.lckback.lckforall.base.api.handler;
 
 import com.lckback.lckforall.base.api.ApiResponse;
 import com.lckback.lckforall.base.api.error.CommonErrorCode;
+import com.lckback.lckforall.base.api.error.ErrorCode;
 import com.lckback.lckforall.base.api.exception.RestApiException;
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Slf4j
 @RestControllerAdvice
-public class ExceptionAdvice {
+public class ExceptionAdvice extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(RestApiException.class)
-    public ApiResponse<?> handleRestApiException(RestApiException e) {
-        return ApiResponse.createFail(e.getErrorCode());
+    public ResponseEntity<Object> handleRestApiException(RestApiException ex) {
+        ErrorCode errorCode = ex.getErrorCode();
+        return handleExceptionInternal(errorCode);
     }
 
-    @ExceptionHandler(Exception.class)
-    public ApiResponse<?> handleException() {
-        return ApiResponse.createFail(CommonErrorCode.INTERNAL_SERVER_ERROR);
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Object> handleIllegalArgument(IllegalArgumentException ex) {
+        log.warn("handleIllegalArgument");
+        ErrorCode errorCode = CommonErrorCode.INVALID_PARAMETER;
+        return handleExceptionInternal(errorCode, ex.getMessage());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        log.warn("handleHttpRequestMethodNotSupportedException");
+        ErrorCode errorCode = CommonErrorCode.METHOD_NOT_ALLOWED;
+        return handleExceptionInternal(errorCode);
+    }
+
+    @Override
+    public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException e, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        log.warn("MethodArgumentNotValidException ");
+        ErrorCode errorCode = CommonErrorCode.INVALID_PARAMETER;
+        return handleExceptionInternal(errorCode, getDefaultMessage(e));
+    }
+
+    @ExceptionHandler(jakarta.validation.ConstraintViolationException.class)
+    public ResponseEntity<Object> handleConstraintViolation(ConstraintViolationException e) {
+        log.warn("ConstraintViolationException");
+        ErrorCode errorCode = CommonErrorCode.INVALID_PARAMETER;
+        return handleExceptionInternal(errorCode, e.getMessage());
+    }
+
+    private static String getDefaultMessage(MethodArgumentNotValidException e) {
+        StringBuilder message = new StringBuilder();
+        for (ObjectError error : e.getBindingResult().getAllErrors()) {
+            message.append(error.getDefaultMessage()).append("\u00a0");
+        }
+        return message.toString();
+    }
+
+    private ResponseEntity<Object> handleExceptionInternal(final ErrorCode errorCode) {
+        return ResponseEntity.status(errorCode.getHttpStatus()).body(ApiResponse.createFail(errorCode));
+    }
+
+    private ResponseEntity<Object> handleExceptionInternal(final ErrorCode errorCode, final String message) {
+        return ResponseEntity.status(errorCode.getHttpStatus()).body(ApiResponse.createFail(message));
     }
 }
 
