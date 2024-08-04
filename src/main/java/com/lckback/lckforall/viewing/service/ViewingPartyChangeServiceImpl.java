27af417a679b105fdc8924461c20f638b87866ae
JunYeong Park <101688752+farmJun@users.noreply.github.com>
2024-08-05 01:20:43 +0900

    [Feat/#54] update my team (#65)
    * Feat(UpdateMyTeamDto): add DTO for update my team
    
    * Feat(User): implement updateMyTeam()
    
    * Feat(MyPageService): implement updateMyTeam()
    
    * Feat(MyPageController): implement updateMyTeam()
    
    * Feat(TeamErrorCode): add team error code
    
    * Fix:
    
    * Fix(ViewingPartyChangeServiceImpl): fix

--- src/main/java/com/lckback/lckforall/viewing/service/ViewingPartyChangeServiceImpl.java
+++ src/main/java/com/lckback/lckforall/viewing/service/ViewingPartyChangeServiceImpl.java
@@ -1,6 +1,7 @@
 package com.lckback.lckforall.viewing.service;
 
 import com.lckback.lckforall.base.api.error.CommonErrorCode;
+import com.lckback.lckforall.base.api.error.UserErrorCode;
 import com.lckback.lckforall.base.api.exception.RestApiException;
 import com.lckback.lckforall.user.model.User;
 import com.lckback.lckforall.user.respository.UserRepository;
@@ -16,17 +17,19 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ViewingPartyChangeServiceImpl implements ViewingPartyChangeService {
 
-    private final UserRepository userRepository;
-    private final ViewingPartyRepository viewingPartyRepository;
+	private final UserRepository userRepository;
+	private final ViewingPartyRepository viewingPartyRepository;
 
 
-    @Override
-    @Transactional
-    public ChangeViewingPartyDTO.Response createViewingParty(Long userId, ChangeViewingPartyDTO.CreateViewingPartyRequest request) {
-        User user = userRepository.findById(userId).orElseThrow(() -> new RestApiException(CommonErrorCode.USER_NOT_FOUND));
-        ViewingParty viewingParty = ViewingPartyChangeConverter.toViewingParty(request);
-        viewingParty.setUser(user);
-        ViewingParty save = viewingPartyRepository.save(viewingParty);
-        return ViewingPartyChangeConverter.toResponse(save);
-    }
+	@Override
+	@Transactional
+	public ChangeViewingPartyDTO.Response createViewingParty(Long userId,
+		ChangeViewingPartyDTO.CreateViewingPartyRequest request) {
+		User user = userRepository.findById(userId).orElseThrow(() -> new RestApiException(
+			UserErrorCode.USER_NOT_FOUND));
+		ViewingParty viewingParty = ViewingPartyChangeConverter.toViewingParty(request);
+		viewingParty.setUser(user);
+		ViewingParty save = viewingPartyRepository.save(viewingParty);
+		return ViewingPartyChangeConverter.toResponse(save);
+	}
 }
