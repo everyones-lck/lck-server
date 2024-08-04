b9a01aa154a2aa549e3d4008a6e7c1039684f0aa
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-08-05 02:13:39 +0900

    Feat(UserRepository): add existsByKakaoUserId

--- src/main/java/com/lckback/lckforall/user/respository/UserRepository.java
+++ src/main/java/com/lckback/lckforall/user/respository/UserRepository.java
@@ -10,5 +10,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
 	Optional<User> findByKakaoUserId(String kakaoUserId);
 	boolean existsByNickname(String nickname);
-
+	boolean existsByKakaoUserId(String kakaoUserId);
 }
