6d8000c234d75fd64d3f11ac402c9e38634960be
gimgyumin <qmean59@gmail.com>
2024-08-04 23:54:36 +0900

    feat : AboutLckTeamService add method to find player information

--- src/main/java/com/lckback/lckforall/aboutlck/service/AboutLckTeamService.java
+++ src/main/java/com/lckback/lckforall/aboutlck/service/AboutLckTeamService.java
@@ -1,5 +1,7 @@
 package com.lckback.lckforall.aboutlck.service;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -8,13 +10,18 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.lckback.lckforall.aboutlck.converter.team.FindTeamPlayerHistoryConverter;
+import com.lckback.lckforall.aboutlck.converter.team.FindTeamPlayerInformationConverter;
 import com.lckback.lckforall.aboutlck.converter.team.FindTeamRatingBySeasonConverter;
 import com.lckback.lckforall.aboutlck.converter.team.FindTeamRatingHistoryConverter;
 import com.lckback.lckforall.aboutlck.converter.team.FindTeamWinningHistoryConverter;
 import com.lckback.lckforall.aboutlck.dto.team.FindTeamPlayerHistoryDto;
+import com.lckback.lckforall.aboutlck.dto.team.FindTeamPlayerInformationDto;
 import com.lckback.lckforall.aboutlck.dto.team.FindTeamRatingHistoryDto;
 import com.lckback.lckforall.aboutlck.dto.team.FindTeamRatingBySeasonDto;
 import com.lckback.lckforall.aboutlck.dto.team.FindTeamWinningHistoryDto;
+import com.lckback.lckforall.player.model.Player;
+import com.lckback.lckforall.player.model.SeasonTeamPlayer;
+import com.lckback.lckforall.player.repository.SeasonTeamPlayerRepository;
 import com.lckback.lckforall.team.repository.SeasonRepository;
 import com.lckback.lckforall.team.repository.SeasonTeamRepository;
 import com.lckback.lckforall.team.repository.TeamRepository;
@@ -34,6 +41,7 @@ public class AboutLckTeamService {
 	private final TeamRepository teamRepository;
 	private final SeasonRepository seasonRepository;
 	private final SeasonTeamRepository seasonTeamRepository;
+	private final SeasonTeamPlayerRepository seasonTeamPlayerRepository;
 
 	public FindTeamRatingBySeasonDto.Response findTeamRatingBySeason(FindTeamRatingBySeasonDto.Parameter param) {
 		Season season = seasonRepository.findByName(param.getSeasonName())
@@ -44,7 +52,6 @@ public class AboutLckTeamService {
 
 		return FindTeamRatingBySeasonConverter.convertToResponse(seasonTeamList);
 	}
-
 
 	public FindTeamWinningHistoryDto.Response findTeamWinningHistory(FindTeamWinningHistoryDto.Parameter param) {
 		Team team = findTeamByTeamId(param.getTeamId());
@@ -75,9 +82,32 @@ public class AboutLckTeamService {
 		return FindTeamPlayerHistoryConverter.convertToResponse(seasonTeams);
 	}
 
+	public FindTeamPlayerInformationDto.Response findTeamPlayerInformation(
+		FindTeamPlayerInformationDto.Parameter param) {
+		Team team = findTeamByTeamId(param.getTeamId());
+		Season season = findSeasonBySeasonName(param.getSeasonName());
+
+		SeasonTeam seasonTeam = seasonTeamRepository.findBySeasonAndTeam(season, team)
+			.orElseThrow(() -> new RestApiException(CommonErrorCode.RESOURCE_NOT_FOUND));
+
+		List<SeasonTeamPlayer> seasonTeamPlayers = seasonTeamPlayerRepository.findAllBySeasonTeam(seasonTeam);
+		List<Player> players = seasonTeamPlayers.stream()
+			.map(SeasonTeamPlayer::getPlayer)
+			.filter(player -> player.getRole().equals(param.getPlayerRole()))
+			.toList();
+
+		return FindTeamPlayerInformationConverter.convertToResponse(players);
+	}
+
 	// 에러코드 구체화 필요
 	private Team findTeamByTeamId(Long teamId) {
 		return teamRepository.findById(teamId)
+			.orElseThrow(() -> new RestApiException(CommonErrorCode.RESOURCE_NOT_FOUND));
+	}
+
+	// 에러코드 구체화 필요
+	private Season findSeasonBySeasonName(String seasonName) {
+		return seasonRepository.findByName(seasonName)
 			.orElseThrow(() -> new RestApiException(CommonErrorCode.RESOURCE_NOT_FOUND));
 	}
 
