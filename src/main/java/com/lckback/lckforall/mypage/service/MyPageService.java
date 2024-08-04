e2624f1ab3e2005e02a5ef86e69c6c630f222f08
박준영 <wnsdud6969@naver.com>
2024-08-04 23:22:43 +0900

    Feat(MyPageService): implement updateMyTeam()

--- src/main/java/com/lckback/lckforall/mypage/service/MyPageService.java
+++ src/main/java/com/lckback/lckforall/mypage/service/MyPageService.java
@@ -1,13 +1,19 @@
 package com.lckback.lckforall.mypage.service;
 
 import com.lckback.lckforall.base.api.error.CommonErrorCode;
+import com.lckback.lckforall.base.api.error.TeamErrorCode;
 import com.lckback.lckforall.base.api.error.UserErrorCode;
 import com.lckback.lckforall.base.api.exception.RestApiException;
 import com.lckback.lckforall.mypage.dto.GetUserProfileDto;
+import com.lckback.lckforall.mypage.dto.UpdateMyTeamDto;
 import com.lckback.lckforall.mypage.dto.UpdateUserProfileDto;
+import com.lckback.lckforall.team.model.Team;
+import com.lckback.lckforall.team.repository.TeamRepository;
 import com.lckback.lckforall.user.model.User;
 import com.lckback.lckforall.user.respository.UserRepository;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.stereotype.Service;
@@ -20,6 +26,8 @@ import org.springframework.web.multipart.MultipartFile;
 public class MyPageService {
 
 	private final UserRepository userRepository;
+
+	private final TeamRepository teamRepository;
 
 	public GetUserProfileDto.Response getUserProfile(Long userId) {
 
@@ -40,8 +48,8 @@ public class MyPageService {
 			.orElseThrow(() -> new RestApiException(CommonErrorCode.RESOURCE_NOT_FOUND));
 
 		user.withdrawFromAccount();
-  }
-  
+	}
+
 	public UpdateUserProfileDto.Response updateUserProfile(
 		Long userId,
 		MultipartFile profileImage,
@@ -73,5 +81,21 @@ public class MyPageService {
 			.updatedProfileImageUrl(user.getProfileImageUrl())
 			.build();
 	}
-  
+
+	public void updateMyTeam(Long userId, UpdateMyTeamDto.Request request) {
+
+		User user = userRepository.findById(userId)
+			.orElseThrow(() -> new RestApiException(UserErrorCode.NOT_EXIST_USER));
+
+		Team team = teamRepository.findById(request.getTeamId())
+			.orElseThrow(() -> new RestApiException(TeamErrorCode.NOT_EXISTS_TEAM));
+
+		LocalDateTime oneMonthAgo = LocalDateTime.now().minus(1, ChronoUnit.MONTHS);
+
+		if (user.getLastUpdatedMyTeam().isAfter(oneMonthAgo)) {
+			throw new RestApiException(TeamErrorCode.MY_TEAM_CANNOT_UPDATE);
+		}
+
+		user.updateMyTeam(team);
+	}
 }
