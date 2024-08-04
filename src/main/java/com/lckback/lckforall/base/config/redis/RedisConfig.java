65cc338e1f1adbf28597e8cff51029d6a0f9c578
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-08-04 15:45:19 +0900

    Style(RedisConfig): comment that generalizes the transformation type to ObjectMapper

--- src/main/java/com/lckback/lckforall/base/config/redis/RedisConfig.java
+++ src/main/java/com/lckback/lckforall/base/config/redis/RedisConfig.java
@@ -35,4 +35,53 @@ public class RedisConfig {
 
 }
 
-
+// ObjectMapper 변환타입 일반화
+// 계속 에러나서 추후에 수정 예정 일단 주석처리
+// package com.lckback.lckforall.base.config.redis;
+//
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.data.redis.connection.RedisConnectionFactory;
+// import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+// import org.springframework.data.redis.core.RedisTemplate;
+// import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+// import org.springframework.data.redis.serializer.StringRedisSerializer;
+// import com.fasterxml.jackson.databind.ObjectMapper;
+// import com.fasterxml.jackson.annotation.JsonAutoDetect;
+// import com.fasterxml.jackson.annotation.PropertyAccessor;
+//
+// @Configuration
+// public class RedisConfig {
+//
+//     @Value("${spring.data.redis.host}")
+//     private String redisHost;
+//
+//     @Value("${spring.data.redis.port}")
+//     private int redisPort;
+//
+//     @Bean
+//     public RedisConnectionFactory redisConnectionFactory() {
+//         return new LettuceConnectionFactory(redisHost, redisPort);
+//     }
+//
+//     @Bean
+//     public RedisTemplate<String, Object> redisTemplate() {
+//         RedisTemplate<String, Object> template = new RedisTemplate<>();
+//         template.setConnectionFactory(redisConnectionFactory());
+//         template.setKeySerializer(new StringRedisSerializer());
+//         template.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper()));
+//         return template;
+//     }
+//
+//     @Bean
+//     public ObjectMapper objectMapper() {
+//         ObjectMapper objectMapper = new ObjectMapper();
+//         objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+//         objectMapper.activateDefaultTyping(
+//             objectMapper.getPolymorphicTypeValidator(),
+//             ObjectMapper.DefaultTyping.OBJECT_AND_NON_CONCRETE
+//         );
+//         return objectMapper;
+//     }
+// }
