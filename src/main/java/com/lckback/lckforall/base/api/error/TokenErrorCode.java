65f879cfa8b9bbe70043428faed4f68b6d3d6942
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-08-05 17:19:05 +0900

    Feat(TokenErrorCode): add token error code
    - EXPIRED_REFRESH_TOKEN
    - MALFORMED_JWT
    - SIGNATURE_INVALID

--- src/main/java/com/lckback/lckforall/base/api/error/TokenErrorCode.java
+++ src/main/java/com/lckback/lckforall/base/api/error/TokenErrorCode.java
@@ -11,7 +11,10 @@ public enum TokenErrorCode implements ErrorCode {
 
 	INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 Access 토큰입니다."),
 	INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 Refresh 토큰입니다."),
-	EXPIRED_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 Access 토큰입니다.");
+	EXPIRED_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 Access 토큰입니다."),
+	EXPIRED_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 Refresh 토큰입니다."),
+	MALFORMED_JWT(HttpStatus.BAD_REQUEST, "잘못된 JWT 토큰입니다."),
+	SIGNATURE_INVALID(HttpStatus.UNAUTHORIZED, "유효하지 않은 JWT 서명입니다.");
 
 	private final HttpStatus httpStatus;
 	private final String message;
