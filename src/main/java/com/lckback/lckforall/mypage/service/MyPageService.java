3a980fe8a238b02ee8c1af28935fd0acda17d6d5
박준영 <wnsdud6969@naver.com>
2024-08-05 18:31:32 +0900

    Feat(MyPageService): implement viewing party find, delete

--- src/main/java/com/lckback/lckforall/mypage/service/MyPageService.java
+++ src/main/java/com/lckback/lckforall/mypage/service/MyPageService.java
@@ -3,22 +3,31 @@ package com.lckback.lckforall.mypage.service;
 import com.lckback.lckforall.base.api.error.CommonErrorCode;
 import com.lckback.lckforall.base.api.error.TeamErrorCode;
 import com.lckback.lckforall.base.api.error.UserErrorCode;
+import com.lckback.lckforall.base.api.error.ViewingPartyErrorCode;
 import com.lckback.lckforall.base.api.exception.RestApiException;
 import com.lckback.lckforall.community.model.Comment;
 import com.lckback.lckforall.community.model.Post;
 import com.lckback.lckforall.community.repository.CommentRepository;
 import com.lckback.lckforall.community.repository.PostRepository;
+import com.lckback.lckforall.mypage.dto.DeleteParticipationDto;
+import com.lckback.lckforall.mypage.dto.DeleteViewingPartyDto;
 import com.lckback.lckforall.mypage.dto.GetUserCommentDto;
 import com.lckback.lckforall.mypage.dto.GetUserPostDto;
 import com.lckback.lckforall.mypage.dto.GetUserProfileDto;
+import com.lckback.lckforall.mypage.dto.GetViewingPartyDto;
 import com.lckback.lckforall.mypage.dto.UpdateMyTeamDto;
 import com.lckback.lckforall.mypage.dto.UpdateUserProfileDto;
 import com.lckback.lckforall.team.model.Team;
 import com.lckback.lckforall.team.repository.TeamRepository;
 import com.lckback.lckforall.user.model.User;
 import com.lckback.lckforall.user.respository.UserRepository;
+import com.lckback.lckforall.viewing.model.Participate;
+import com.lckback.lckforall.viewing.model.ViewingParty;
+import com.lckback.lckforall.viewing.repository.ParticipateRepository;
+import com.lckback.lckforall.viewing.repository.ViewingPartyRepository;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -43,6 +52,10 @@ public class MyPageService {
 	private final PostRepository postRepository;
 
 	private final CommentRepository commentRepository;
+
+	private final ParticipateRepository participateRepository;
+
+	private final ViewingPartyRepository viewingPartyRepository;
 
 	public GetUserProfileDto.Response getUserProfile(Long userId) {
 
@@ -140,6 +153,71 @@ public class MyPageService {
 			.build();
 	}
 
+	public GetViewingPartyDto.Response getUserViewingPartyAsParticipate(
+		Long userId,
+		Pageable pageable) {
+
+		User user = userRepository.findById(userId)
+			.orElseThrow(() -> new RestApiException(UserErrorCode.NOT_EXIST_USER));
+
+		Page<Participate> participates = participateRepository.findByUser(user, pageable);
+
+		Page<ViewingParty> viewingParties = participates
+			.map(participate -> participate.getViewingParty());
+
+		return GetViewingPartyDto.Response.builder()
+			.viewingParties(convertToViewingPartiesInformation(viewingParties))
+			.isLast(viewingParties.isLast())
+			.build();
+	}
+
+	public GetViewingPartyDto.Response getUserViewingPartyAsHost(Long userId, Pageable pageable) {
+
+		User user = userRepository.findById(userId)
+			.orElseThrow(() -> new RestApiException(UserErrorCode.NOT_EXIST_USER));
+
+		Page<ViewingParty> viewingParties = viewingPartyRepository.findByUser(user, pageable);
+
+		return GetViewingPartyDto.Response.builder()
+			.viewingParties(convertToViewingPartiesInformation(viewingParties))
+			.isLast(viewingParties.isLast())
+			.build();
+	}
+
+	public void cancelViewingPartyParticipation(
+		Long userId,
+		DeleteParticipationDto.Request request) {
+
+		User user = userRepository.findById(userId)
+			.orElseThrow(() -> new RestApiException(UserErrorCode.NOT_EXIST_USER));
+
+		ViewingParty viewingParty = viewingPartyRepository.findById(request.getViewingPartyId())
+			.orElseThrow(() -> new RestApiException(CommonErrorCode.RESOURCE_NOT_FOUND));
+
+		Participate participate =
+			participateRepository.findByViewingPartyAndUser(viewingParty, user)
+				.orElseThrow(() -> new RestApiException(CommonErrorCode.RESOURCE_NOT_FOUND));
+
+		participateRepository.delete(participate);
+	}
+
+	public void cancelViewingPartyHosting(
+		Long userId,
+		DeleteViewingPartyDto.Request request) {
+
+		User user = userRepository.findById(userId)
+			.orElseThrow(() -> new RestApiException(UserErrorCode.NOT_EXIST_USER));
+
+		ViewingParty viewingParty = viewingPartyRepository.findById(request.getViewingPartyId())
+			.orElseThrow(() -> new RestApiException(CommonErrorCode.RESOURCE_NOT_FOUND));
+
+		if (!viewingParty.getUser().equals(user)) {
+			throw new RestApiException(ViewingPartyErrorCode.USER_IS_NOT_HOST);
+		}
+
+		viewingPartyRepository.delete(viewingParty);
+	}
+
 	private List<GetUserPostDto.Information> convertToPostInformation(Page<Post> posts) {
 		return posts.stream()
 			.map(post -> GetUserPostDto.Information.builder()
@@ -161,4 +239,15 @@ public class MyPageService {
 			.collect(Collectors.toList());
 	}
 
+	private List<GetViewingPartyDto.Information> convertToViewingPartiesInformation(
+		Page<ViewingParty> viewingParties) {
+
+		return viewingParties.stream()
+			.map(viewingParty -> GetViewingPartyDto.Information.builder()
+				.id(viewingParty.getId())
+				.name(viewingParty.getName())
+				.date(viewingParty.getDate().format(DateTimeFormatter.ofPattern("yyyy.MM.dd")))
+				.build()
+			).collect(Collectors.toList());
+	}
 }
