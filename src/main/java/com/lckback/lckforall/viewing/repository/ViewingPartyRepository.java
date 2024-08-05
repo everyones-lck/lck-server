3f3937a918e6727633224584df8aa5d97bcc366a
박준영 <wnsdud6969@naver.com>
2024-08-05 18:29:29 +0900

    Feat(ViewingPartyRepository): add findByUser()

--- src/main/java/com/lckback/lckforall/viewing/repository/ViewingPartyRepository.java
+++ src/main/java/com/lckback/lckforall/viewing/repository/ViewingPartyRepository.java
@@ -7,5 +7,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ViewingPartyRepository extends JpaRepository<ViewingParty, Long> {
-    Page<ViewingParty> findAllByOrderByCreatedAtDesc(Pageable pageable);
+
+	Page<ViewingParty> findAllByOrderByCreatedAtDesc(Pageable pageable);
+
+	Page<ViewingParty> findByUser(User user, Pageable pageable);
+
 }
