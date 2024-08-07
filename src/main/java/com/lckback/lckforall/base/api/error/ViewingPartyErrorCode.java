efdac8cf6341adb82e645520c7f39bf922b4913a
JunYeong Park <101688752+farmJun@users.noreply.github.com>
2024-08-07 18:01:15 +0900

    Feat(ViewingPartyErrorCode): add error code

--- src/main/java/com/lckback/lckforall/base/api/error/ViewingPartyErrorCode.java
+++ src/main/java/com/lckback/lckforall/base/api/error/ViewingPartyErrorCode.java
@@ -11,7 +11,8 @@ public enum ViewingPartyErrorCode implements ErrorCode {
     PARTY_NOT_FOUND(HttpStatus.NOT_FOUND, "뷰잉파티를 찾을수 없습니다."),
     FAILED_PARTICIPATE_PARTY(HttpStatus.NOT_ACCEPTABLE, "뷰잉파티 참여에 실패했습니다."),
     FAILED_UPDATE_VIEWING_PARTY(HttpStatus.NOT_ACCEPTABLE, "뷰잉파티 글 수정에 실패했습니다."),
-    OWNER_VIEWING_PARTY_NOT_FOUND(HttpStatus.NOT_FOUND, "개최자가 생성한 뷰잉파티 글을 찾을수 없습니다.");
+    OWNER_VIEWING_PARTY_NOT_FOUND(HttpStatus.NOT_FOUND, "개최자가 생성한 뷰잉파티 글을 찾을수 없습니다."),
+    USER_IS_NOT_HOST(HttpStatus.BAD_REQUEST, "호스트가 아니라 뷰잉 파티 개최 취소 불가합니다.");
     private final HttpStatus httpStatus;
     private final String message;
 
