81fd3891966e0b577fe6f9ade66b89fcd91e6c93
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-08-08 22:52:14 +0900

    Refactor: modify code related swagger
    - AuthController
    - JWTFilter
    - SignupUserDataConverter
    - SignupUserDataDto

--- src/main/java/com/lckback/lckforall/user/converter/SignupUserDataConverter.java
+++ src/main/java/com/lckback/lckforall/user/converter/SignupUserDataConverter.java
@@ -10,20 +10,6 @@ import com.lckback.lckforall.user.model.User;
 
 public class SignupUserDataConverter {
 
-    // 회원가입 성공한 유저 데이터를 다시 클라이언트로 보내는 로직
-    // 현재 쓰지 않는 데이터라 주석처리해둠
-    //    public static SignupUserDataDto.SignupUserData convertToSignupUserData(User user) {
-    //        return SignupUserDataDto.SignupUserData.builder()
-    //            .id(user.getId())
-    //            .kakaoUserId(user.getKakaoUserId())
-    //            .nickName(user.getNickname())
-    //            .profileImageUrl(user.getProfileImageUrl())
-    //            .role(user.getRole())
-    //            .status(user.getStatus())
-    //            .team(user.getTeam())
-    //            .build();
-    //    }
-
     // SignupUserData DTO 객체를 User 엔티티 객체로 변환하기 위해 사용
     // 클라이언트로부터 전달받은 회원가입 데이터를 데이터베이스에 저장하기 위해 User 엔티티로 변환할 때 사용
     public static User convertToUser(SignupUserDataDto.SignupUserData signupUserData, String profileImageUrl, Team team) {
