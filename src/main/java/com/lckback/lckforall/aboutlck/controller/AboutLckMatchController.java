da101205548a4950a499601064312434285265f1
gimgyumin <qmean59@gmail.com>
2024-08-11 01:43:14 +0900

    refactor : edit api path

--- src/main/java/com/lckback/lckforall/aboutlck/controller/AboutLckMatchController.java
+++ src/main/java/com/lckback/lckforall/aboutlck/controller/AboutLckMatchController.java
@@ -22,7 +22,7 @@ public class AboutLckMatchController {
 
 	private final AboutLckMatchService aboutLckMatchService;
 
-	@GetMapping("/")
+	@GetMapping
 	public ApiResponse<FindMatchesByDateDto.Response> findMatchInformationByDate(
 		@RequestParam("searchDate") LocalDate searchDate) {
 		FindMatchesByDateDto.Parameter param = FindMatchesByDateDto.Parameter.builder().searchDate(searchDate).build();
