2637c110041dd206319bcc149c2ad81e7dca3d8b
qmean <77445214+qmean@users.noreply.github.com>
2024-08-11 15:30:18 +0900

    [Refactor#116] swagger pageable처리 및 api tag, opertaion 추가 (커스텀 어노테이션 활용) (#119)
    * feat(SwaggerPageable) : custom annotation to display pageable to query param in swagger
    
    * feat : add operations and pageable annotation in aboutLckController

--- src/main/java/com/lckback/lckforall/aboutlck/controller/AboutLckPlayerController.java
+++ src/main/java/com/lckback/lckforall/aboutlck/controller/AboutLckPlayerController.java
@@ -11,18 +11,23 @@ import com.lckback.lckforall.aboutlck.dto.player.FindPlayerTeamHistoryDto;
 import com.lckback.lckforall.aboutlck.dto.player.FindPlayerWinningHistoryDto;
 import com.lckback.lckforall.aboutlck.service.AboutLckPlayerService;
 import com.lckback.lckforall.base.api.ApiResponse;
+import com.lckback.lckforall.base.setting.SwaggerPageable;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/aboutlck/player")
 @RequiredArgsConstructor
 @SecurityRequirement(name = "JWT Token")
+@Tag(name = "AboutLckPlayer", description = "AboutLck 선수 정보 조회 API")
 public class AboutLckPlayerController {
 
 	private final AboutLckPlayerService aboutLckPlayerService;
 
+	@Operation(summary = "선수 정보 조회 API", description = "특정 선수의 정보를 조회합니다.")
 	@GetMapping("/{playerId}/information")
 	public ApiResponse<FindPlayerInformationDto.Response> findPlayerInformation(
 		@PathVariable("playerId") Long playerId) {
@@ -35,6 +40,8 @@ public class AboutLckPlayerController {
 		return ApiResponse.createSuccess(data);
 	}
 
+	@SwaggerPageable
+	@Operation(summary = "선수 팀 이력 조회 API", description = "특정 선수의 팀 이력을 조회합니다.")
 	@GetMapping("/{playerId}/team-history")
 	public ApiResponse<FindPlayerTeamHistoryDto.Response> findPlayerTeamHistory(
 		@PathVariable("playerId") Long playerId,
@@ -48,6 +55,8 @@ public class AboutLckPlayerController {
 		return ApiResponse.createSuccess(data);
 	}
 
+	@SwaggerPageable
+	@Operation(summary = "선수 우승 기록 조회 API", description = "특정 선수의 우승 기록을 조회합니다.")
 	@GetMapping("/{playerId}/winning-history")
 	public ApiResponse<FindPlayerWinningHistoryDto.Response> findPlayerWinningHistory(
 		@PathVariable("playerId") Long playerId,
