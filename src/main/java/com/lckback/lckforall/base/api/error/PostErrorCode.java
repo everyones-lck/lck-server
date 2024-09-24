461e8adf235391bea650d370ce9c87c8d7d84eaf
gimgyumin <qmean59@gmail.com>
2024-09-24 15:40:04 +0900

    feat: add field to validate empty file list

--- src/main/java/com/lckback/lckforall/base/api/error/PostErrorCode.java
+++ src/main/java/com/lckback/lckforall/base/api/error/PostErrorCode.java
@@ -10,7 +10,8 @@ public enum PostErrorCode implements ErrorCode {
     POST_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 게시물이 없습니다."),
 	POST_TYPE_NOT_FOUND(HttpStatus.NOT_FOUND, "포스트 타입을 찾을 수 없습니다."),
 	POST_FILE_NOT_FOUND(HttpStatus.NOT_FOUND, "포스트 파일을 찾을 수 없습니다."),
-	POST_NOT_VALIDATE(HttpStatus.FORBIDDEN, "해당 유저가 작성한 게시글이 아닙니다."), //질문 forbidden , notfound
+	POST_NOT_VALIDATE(HttpStatus.FORBIDDEN, "해당 유저가 작성한 게시글이 아닙니다."),
+	FILE_IS_EMPTY(HttpStatus.BAD_REQUEST, "비어있는 파일이 있습니다."),
 	POST_ALREADY_REPORTED(HttpStatus.NOT_FOUND, "이미 신고한 게시글입니다");
 	private final HttpStatus httpStatus;
 	private final String message;
